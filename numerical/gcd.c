b892ace6e9cad89a0829ad30bd6b6875763f2c23
Vijay Talluru <vijaytalluru@gmail.com>
2013-08-22 20:39:54 -0500

    committing numerical/gcd.c

--- numerical/gcd.c
+++ numerical/gcd.c
@@ -19,7 +19,7 @@ int gcd(int u, int v)
 int main()
 {
   int x,y;
-  while(scanf("%d %d",&x, &y) != EOF)
+  while((printf("Enter two numbers whose GCD is to be calculated\n"),scanf("%d %d",&x, &y) != EOF))
   {
     if (x > 0 && y > 0)
       printf("GCD of %d and %d is %d \n",x,y,gcd(x,y));
